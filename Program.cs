ce4f3fdc11c5be4a76393736660d10403ec9b5ab
Verain <verainthakur1183@gmail.com>
2021-12-28 20:33:55 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace firstDemo
+{
+    class Mobile
+    {
+        int RAM;
+        string motherboard;
+        string speaker;
+
+        public void mobileBoot() //Encapsulation..Wrapper of private variable inside the function
+        {
+            RAM = 2;
+            motherboard = "Initialised";
+            speaker = "Initialisd";
+        }
+    
+
+    }
+
+class user
+{
+    public static void Main()
+    {
+        Mobile samsung = new Mobile();
+        samsung.mobileBoot(); //Abstraction..  operational part of the application like (volume +-, Reset, power)
+    }
+
+}
+}
